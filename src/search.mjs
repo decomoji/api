2b0fe46671d535c374828e16b191b35c1299d383
oti <otiext@gmail.com>
2023-03-06 21:58:28 +0900

    feat: src/search.mjs, import `DecomojiDB`

--- src/search.mjs
+++ src/search.mjs
@@ -1,21 +1,14 @@
 import express from "express";
+import DecomojiDB from "./models/DecomojiDB.mjs";
 
 const app = express();
 
-const data = [
-  { id: 1, name: 'John' },
-  { id: 2, name: 'Jane' },
-  { id: 3, name: 'Bob' },
-];
-
 app.get("/search", (req, res) => {
-  const query = req.query.q
-  const result = data.filter(item => item.name.includes(query));
-  res.json(result);
+  res.json(DecomojiDB.filter((v) => RegExp(req.query.q).test(v.name)));
 });
 
 app.listen(3000, () => {
-  console.log("API server started on port 3000");
+  console.log("decomoji-api server started");
 });
 
-export default app
+export default app;
