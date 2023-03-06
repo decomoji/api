8a0007bc1e1dedb41dfeaeb54e22633f1cf07a35
oti <otiext@gmail.com>
2023-03-07 00:00:23 +0900

    feat: index.mjs, 前実装を index.mjs に集めた

--- index.mjs
+++ index.mjs
@@ -1,1 +1,70 @@
-import app from "./src/search.mjs";
+import express from "express";
+import DecomojiAll from "decomoji/configs/v5_all.json" assert { type: "json" };
+
+const isFalsyString = (v) => {
+  return v === "" || v === null || v === undefined;
+}
+
+// v5_all.json にカテゴリープロapティを加えたものをDBとする
+const DecomojiDB = DecomojiAll.map((v) =>
+  v.path.match(/basic/)
+    ? {
+        ...v,
+        category: "basic",
+      }
+    : v.path.match(/explicit/)
+    ? {
+        ...v,
+        category: "explicit",
+      }
+    : v.path.match(/extra/)
+    ? {
+        ...v,
+        category: "extra",
+      }
+    : v
+);
+
+// query に対してマッチ
+const matches = ({
+  category: decomojiCategory,
+  created: decomojiCreated,
+  name: decomojiName,
+  updated: decomojiUpdated,
+}, {
+  category: queryCategory,
+  created: queryCreated,
+  name: queryName,
+  updated: queryUpdated,
+}) => {
+  // 受け入れ可能なパラメータのいずれかが存在するか
+  const isValidParams = queryCategory || queryCreated || queryName || queryUpdated;
+
+  // name パラメータが decomoji.name に正規表現で一致するか、または空文字か、または null か
+  const nameMatches = RegExp(queryName).test(decomojiName) || isFalsyString(queryName);
+
+  // category パラメータが decomoji.category に一致するか、または空文字か、または null か
+  const categoryMatches = queryCategory === decomojiCategory || isFalsyString(queryCategory);
+
+  // created パラメータが decomoji.created に一致するか、または空文字か、または null か
+  const createdMatches = queryCreated === decomojiCreated || isFalsyString(queryCreated);
+
+  // 修正バージョンが、表示するバージョンであるか否か、または何も選択されていないか否か
+  const updatedMatches = decomojiUpdated
+    ? queryUpdated === decomojiUpdated || isFalsyString(queryCreated)
+    : false;
+
+  return (
+    isValidParams && nameMatches && categoryMatches && (createdMatches || updatedMatches)
+  );
+};
+
+const app = express();
+
+app.get("/search", (req, res) => {
+  return res.json(DecomojiDB.filter((v) => matches(v, req.query)))
+});
+
+app.listen(3000, () => {
+  console.log("decomoji-api server started: http://localhost:3000/search?name=");
+});
